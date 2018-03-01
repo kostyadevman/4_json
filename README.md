580fedce587d007af0da43f2a7cb29ab474097ff
dolly <timohakt@yandex.ru>
2018-03-01 20:59:25 +0300

    emplamented 2 function

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 # Prettify JSON
 
-[TODO. There will be project description]
+pprint_json.py provides a capability to "pretty-print" arbitrary JSON data structures in a well-formatted and more readable way!
 
 # Quickstart
+Content of raw JSON file:
 
-[TODO]
-
+```bash
+{"foo":1,"bar":{"baz":2,"fob":3},"nop":[1,2,3,4]}
+```
 Example of script launch on Linux, Python 3.5:
 
 ```bash
 
 $ python pprint_json.py <path to file>
-# TODO add output example
+{
+    "foo": 1,
+    "bar": {
+        "baz": 2,
+        "fob": 3
+    },
+    "nop": [1, 2, 3, 4]
+}
 
 ```
 
--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,30 @@
 import json
-
+import sys
+import os
 
 def load_data(filepath):
-    pass
-
+    if os.path.isfile(filepath):
+        with open(filepath, 'r') as f:
+            try:
+                raw_json_data = json.loads(f.read())
+            except ValueError as  e:
+                print('Json open failed:')
+                print(e)
+                exit(2)
+        return raw_json_data
+    else:
+        print("File '{}' doesn't exists".format(filepath))
+        exit(1)
 
 def pretty_print_json(data):
-    pass
+    pretty_json_data = json.dumps(data, ensure_ascii=False, indent=4)
+    print(pretty_json_data)
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) < 2:
+        print('Usage: python pprint_json.py <path to file>')
+    else:
+        data = load_data(sys.argv[1])
+        pretty_print_json(data)
+
