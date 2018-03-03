b76677861b061210c30d949bdbe00ed4f5e6e68c
dolly <timohakt@yandex.ru>
2018-03-03 03:28:56 +0300

    emplamented 2 function

--- pprint_json.py
+++ pprint_json.py
@@ -20,7 +20,7 @@ if __name__ == '__main__':
         print('Usage: python pprint_json.py <path to file>')
     else:
         json_data = load_data(sys.argv[1])
-        if json_data is not None:
+        if json_data:
             pretty_json_data = pretty_print_json(json_data)
             print(pretty_json_data)
         else:
