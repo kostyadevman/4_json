8a37c6eca5f6b5b0fe1437ff3d3ddc2de891315c
dolly <timohakt@yandex.ru>
2018-03-03 06:42:58 +0300

    Fix load_data function

--- pprint_json.py
+++ pprint_json.py
@@ -2,18 +2,19 @@ import json
 import sys
 import os
 
+
 def load_data(filepath):
     if os.path.isfile(filepath):
         with open(filepath, 'r') as infile:
             json_content = json.loads(infile.read())
         return json_content
-    else:
-        return None
+
 
 def pretty_print_json(json_content):
     pretty_json_content = json.dumps(json_content, ensure_ascii=False, indent=4)
 
     return pretty_json_content
+
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
@@ -24,5 +25,5 @@ if __name__ == '__main__':
             pretty_json_content = pretty_print_json(json_content)
             print(pretty_json_content)
         else:
-            print('File doesn\'t exist')
+            print("File doesn't exist")
 
