af52b87ad44bd1c450571b1e4b3d44254f2be811
dolly <timohakt@yandex.ru>
2018-03-03 03:26:57 +0300

    emplamented 2 function

--- pprint_json.py
+++ pprint_json.py
@@ -4,28 +4,25 @@ import os
 
 def load_data(filepath):
     if os.path.isfile(filepath):
-        with open(filepath, 'r') as outfile:
-            try:
-                raw_json_data = json.loads(outfile.read())
-            except ValueError as  e:
-                print('Json open failed:')
-                print(e)
-                return None
-        return raw_json_data
+        with open(filepath, 'r') as infile:
+            json_data = json.loads(infile.read())
+        return json_data
     else:
-        print("File '{}' doesn't exists".format(filepath))
         return None
 
 def pretty_print_json(json_data):
     pretty_json_data = json.dumps(json_data, ensure_ascii=False, indent=4)
-    print(pretty_json_data)
 
+    return pretty_print_json()
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
         print('Usage: python pprint_json.py <path to file>')
     else:
         json_data = load_data(sys.argv[1])
-        if json_data != None:
-            pretty_print_json(json_data)
+        if json_data is not None:
+            pretty_json_data = pretty_print_json(json_data)
+            print(pretty_json_data)
+        else:
+            print('File doesn\'t exist')
 
